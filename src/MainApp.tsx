433bebc9e25f5250a091de2321ccbcf78755680b
minung.han <hmu332233@gmail.com>
2021-08-16 18:19:54 +0900

    chore: parcel, ts, react 설정 추가

---
+++ src/MainApp.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function MainApp(props) {
+  return (
+    <div>
+      테스트
+    </div>
+  );
+}
+
+MainApp.propTypes = {
+};
+MainApp.defaultProps = {
+};
+
+export default MainApp;
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MainApp from './MainApp';
+
+ReactDOM.render( <MainApp /> , document.getElementById('app'));
