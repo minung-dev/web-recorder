ba19d5c1e5f4abed90967527830922e58bc341a6
minung.han <hmu332233@gmail.com>
2021-08-20 02:50:41 +0900

    feat: Video에 controls props 추가

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
 type VideoProps = {
-  autoPlay: boolean,
+  autoPlay?: boolean,
+  controls?: boolean,
 };
 
-const Video = React.forwardRef(({ autoPlay }: VideoProps, ref: React.Ref<HTMLVideoElement>) => {
+const Video = React.forwardRef(({ autoPlay, controls }: VideoProps, ref: React.Ref<HTMLVideoElement>) => {
   return (
-    <video style={{ width: '100%', backgroundColor: '#000' }} ref={ref} autoPlay={autoPlay} />
+    <video style={{ width: '100%', backgroundColor: '#000' }} ref={ref} autoPlay={autoPlay} controls={controls} />
   );
 });
 
 Video.defaultProps = {
   autoPlay: false,
+  controls: false,
 };
 
 export default Video;
