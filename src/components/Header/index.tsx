8a65fcab1779a04a2cbff9a8050528c7127efeeb
minung.han <hmu332233@gmail.com>
2021-08-21 13:33:51 +0900

    feat: Header dom 구조 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,11 +4,9 @@ type HeaderProps = {};
 
 function Header(props: HeaderProps) {
   return (
-    <nav className="container navbar has-background-primary px-5 py-1" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand is-flex is-align-items-center">
-        <h1 className="title is-4 has-text-white">Screen Recorder</h1>
-      </div>
-    </nav>
+    <div className="container navbar has-background-primary px-5 py-3">
+      <h1 className="title is-4 has-text-white">Screen Recorder</h1>
+    </div>
   );
 }
 
