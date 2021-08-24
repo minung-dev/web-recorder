4754c518ea63c112949f2a292d92050069d08be0
minung.han <hmu332233@gmail.com>
2021-08-25 01:21:42 +0900

    feat: Video에 muted props 추가

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -3,17 +3,19 @@ import React from 'react';
 type VideoProps = {
   autoPlay?: boolean,
   controls?: boolean,
+  muted?: boolean,
 };
 
-const Video = React.forwardRef(({ autoPlay, controls }: VideoProps, ref: React.Ref<HTMLVideoElement>) => {
+const Video = React.forwardRef(({ autoPlay, controls, muted }: VideoProps, ref: React.Ref<HTMLVideoElement>) => {
   return (
-    <video style={{ width: '100%', height: '300px', backgroundColor: '#363636', borderRadius: '4px' }} ref={ref} autoPlay={autoPlay} controls={controls} />
+    <video style={{ width: '100%', height: '300px', backgroundColor: '#363636', borderRadius: '4px' }} ref={ref} autoPlay={autoPlay} controls={controls} muted={muted} />
   );
 });
 
 Video.defaultProps = {
   autoPlay: false,
   controls: false,
+  muted: false,
 };
 
 export default Video;
