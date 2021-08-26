d1e07bae5fbd6f3565716ff0ec7737c8808a6de0
minung.han <hmu332233@gmail.com>
2021-08-27 03:42:15 +0900

    feat: useRecording에 downloadGif 기능 추가

--- src/hooks/useRecording.ts
+++ src/hooks/useRecording.ts
@@ -1,5 +1,8 @@
 import { useState, useRef } from 'react';
 
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+const ffmpeg = createFFmpeg({ log: true });
+
 const constraints = {
   video: true,
   audio: true,
@@ -16,6 +19,7 @@ function useRecording() {
   const mediaStream = useRef<MediaStream | null>(null);
   const mediaRecorder = useRef<MediaRecorder | null>(null);
   const recordedChunks = useRef<Blob[]>([]);
+
 
   if (!MediaRecorder.isTypeSupported(options.mimeType)) {
     alert('지원하지 않는 브라우저입니다.');
@@ -64,6 +68,7 @@ function useRecording() {
     getLiveStream: () => mediaStream.current,
     getVideoUrl: () => getVideoUrl(recordedChunks.current),
     downloadVideo: (fileName: string = 'video') => downloadVideo(recordedChunks.current, fileName),
+    downloadGif: (fileName: string = 'video') => downloadGif(recordedChunks.current, fileName),
   };
 }
 
@@ -79,6 +84,35 @@ function downloadVideo(chunks: Blob[], fileName: string) {
 	aElement.href = URL.createObjectURL(blob);
 	aElement.download = `${fileName}.webm`;
 	aElement.click();
+}
+
+async function downloadGif(chunks: Blob[], fileName: string) {
+  const url = await convertToGif(chunks);
+  const aElement = document.createElement('a');
+	aElement.href = url;
+	aElement.download = `${fileName}.gif`;
+	aElement.click();
+}
+
+// TODO: worker에서 동작하도록 수정
+async function convertToGif (chunks: Blob[]) {
+  const blob = new Blob(chunks, { type: 'video/webm' });
+
+  await ffmpeg.load();
+
+  // 메모리에 파일 올리기
+  ffmpeg.FS('writeFile', 'test.webm', await fetchFile(blob));
+  
+  // gif로 변환
+  await ffmpeg.run('-i', 'test.webm', '-r', '10', 'out.gif');
+  // TODO: mp4 다운로드 기능 지원
+  // await ffmpeg.run('-i', 'test.webm', '-c:a', 'libopus', '-b:a', '96K', '-strict', 'out.mp4');
+
+  // 파일 불러오기
+  const data = ffmpeg.FS('readFile', 'out.gif');
+
+  const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
+  return url;
 }
 
 export default useRecording;
