0cc65b5d7bfedcea2763c2115518cbb79ab47e61
minung.han <hmu332233@gmail.com>
2021-08-28 00:03:43 +0900

    feat: Button에 loading props 추가

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,12 +7,13 @@ type ButtonProps = {
   className?: string,
   color?: string,
   outline?: boolean,
+  loading?: boolean,
   onClick?: React.MouseEventHandler<HTMLButtonElement>,
 };
 
-function Button({ children, className, color, outline, onClick }: ButtonProps) {
+function Button({ children, className, color, outline, loading, onClick }: ButtonProps) {
   return (
-    <button className={classNames(className, `button is-${color} is-fullwidth`, outline && 'is-outlined')} onClick={onClick}>
+    <button className={classNames(className, `button is-${color} is-fullwidth`, outline && 'is-outlined', loading && 'is-loading')} onClick={onClick}>
       {children}
     </button>
   );
