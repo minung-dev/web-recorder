7a5f271dc464f687e12e2790f932fb69910aa6cf
minung.han <hmu332233@gmail.com>
2021-08-29 01:27:05 +0900

    refactor: DownloadPanel에서 Select 부분 공통 컴포넌트로 변경

--- src/components/DownloadPanel/index.tsx
+++ src/components/DownloadPanel/index.tsx
@@ -2,18 +2,28 @@ import React, { useState } from 'react';
 import styles from './styles.module.css';
 
 import Button from '../Button';
+import Select from '../Select';
 
 import classnames from 'classnames';
 
-type DownloadPanelProps = {
-  onDownloadClick: (type: string) => void | Promise<void>,
-};
+const FORMAT_ITEMS = [{
+  name: '비디오',
+  value: 'video',
+}, {
+  name: 'GIF',
+  value: 'gif',
+}];
 
+type DownloadPanelProps = {
+  onDownloadClick: (type: string) => Promise<void>,
+};
 function DownloadPanel({ onDownloadClick }: DownloadPanelProps) {
   const [isLoading, setIsLoading] = useState(false);
+  const [format, setFormat] = useState(FORMAT_ITEMS[0].value);
+
   const handleDownloadClick = async () => {
     setIsLoading(true);
-    await onDownloadClick('gif');
+    await onDownloadClick(format);
     setIsLoading(false);
   };
 
@@ -21,10 +31,7 @@ function DownloadPanel({ onDownloadClick }: DownloadPanelProps) {
     <section className={classnames(styles.DownloadPanel, 'section py-3')}>
       <div className="field">
         <label className="label">파일 포멧</label>
-        <div className="control is-flex">
-          <Button color="info">비디오</Button>
-          <Button color="info" outline className="ml-3">GIF</Button>
-        </div>
+        <Select items={FORMAT_ITEMS} selected={format} onChange={setFormat} />
         <p className="help">다운로드할 파일의 포멧을 선택합니다.</p>
       </div>
       <Button color="info" loading={isLoading} onClick={handleDownloadClick}>다운로드</Button>
