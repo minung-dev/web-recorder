ac54a180e6e23b4730468d0a5d5630c90afc3501
minung.han <hmu332233@gmail.com>
2021-09-12 23:59:45 +0900

    canvas에 색 선택하는 기능 추가

--- src/extensions/content.ts
+++ src/extensions/content.ts
@@ -1,3 +1,6 @@
+// TODO: ctx를 다루는 좋은 방법 찾기
+let ctx: CanvasRenderingContext2D;
+
 const Wrapper = () => {
   const wrapper = document.createElement('div');
   wrapper.style.position = 'fixed';
@@ -9,6 +12,37 @@ const Wrapper = () => {
   return wrapper;
 };
 
+const btnStyle = `width: 50px; height: 50px; box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08); border-radius: 25px; cursor: pointer;`;
+
+const Tools = () => {
+  const toolWrapper = document.createElement('div');
+  toolWrapper.style.position = 'absolute';
+  toolWrapper.style.left = '16px';
+  toolWrapper.style.bottom = '16px';
+
+  toolWrapper.innerHTML = `
+    <div style="${btnStyle} background-color:rgb(0, 0, 0)"></div>
+    <div style="${btnStyle} background-color:rgb(255,255,255)"></div>
+    <div style="${btnStyle} background-color:rgb(255,59,48)"></div>
+    <div style="${btnStyle} background-color:rgb(255,149,0)"></div>
+    <div style="${btnStyle} background-color:rgb(255,204,0)"></div>
+    <div style="${btnStyle} background-color:rgb(76,217,100)"></div>
+    <div style="${btnStyle} background-color:rgb(90,200,250)"></div>
+    <div style="${btnStyle} background-color:rgb(0,122,255)"></div>
+    <div style="${btnStyle} background-color:rgb(88,86,214)"></div>
+  `;
+
+  toolWrapper.addEventListener('click', handleColorClick);
+
+  function handleColorClick(e: MouseEvent) {
+    const color = (e.target as HTMLElement).style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+  }
+
+  return toolWrapper;
+};
+
 const Canvas = () => {
   const INITIAL_COLOR = 'rgb(255,59,48)';
 
@@ -16,7 +50,7 @@ const Canvas = () => {
   canvas.width = window.innerWidth;
   canvas.height = window.outerHeight;
 
-  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+  ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
   ctx.strokeStyle = INITIAL_COLOR;
   ctx.fillStyle = INITIAL_COLOR;
   ctx.lineWidth = 2.5;
@@ -54,8 +88,10 @@ const Canvas = () => {
 export const injectApp = () => {
   const wrapper = Wrapper();
   const canvas = Canvas();
+  const tools = Tools();
 
   wrapper.append(canvas);
+  wrapper.append(tools);
   document.body.append(wrapper);
 };
 
