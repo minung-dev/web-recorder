5c2ef98921efbe166344b6f4005772ebe8585246
minung.han <hmu332233@gmail.com>
2021-12-12 23:24:32 +0900

    feat: Progress가 max를 넘길 경우 경고 UI 추가

--- src/client/components/Progress/index.tsx
+++ src/client/components/Progress/index.tsx
@@ -4,39 +4,52 @@ import styles from './styles.module.css';
 import classnames from 'classnames';
 
 type ProgressProps = {
+  on: boolean,
   max: number // max seconds
-  onMaxiumProgress: () => void,
+  // onMaxiumProgress: () => void,
 };
 
 function Progress({
+  on,
   max,
-  onMaxiumProgress,
+  // onMaximumProgress,
 }: ProgressProps) {
+  const [isMax, setIsMax] = useState(false);
   const [value, setValue] = useState(0);
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      if (value === max) {
-        clearInterval(intervalId);
-        onMaxiumProgress();
-      }
 
+  useEffect(() => {
+    if (on === false) {
+      setValue(0);
+      setIsMax(false);
+      return;
+    }
+
+    const intervalId = setInterval(() => {
       setValue(v => v + 1);
     }, 1000);
 
     return () => clearInterval(intervalId);
-  }, []);
+  }, [on]);
+
+  useEffect(() => {
+    if (value >= max) {
+      setIsMax(true);
+      // onMaximumProgress();
+    }
+  }, [value]);
 
   return (
     <div className={styles.Progress}>
-      <span className={classnames(styles.Progress__text, 'is-size-7', 'has-text-weight-medium')}>{value}초 / 최대 {max}초</span>
-      <progress className="progress is-primary is-large" value={value} max={max} />
+      <span className={classnames(styles.Progress__text, 'is-size-7', 'has-text-weight-medium', isMax && 'has-text-white')}>{value}초 / 최대 {max}초</span>
+      <progress className={classnames('progress', 'is-large', isMax ? 'is-danger' : 'is-primary')} value={value} max={max} />
     </div>
   );
 }
 
 Progress.defaultProps = {
+  on: false,
   max: 600,
-  onMaxiumProgress: () => {},
+  // onMaximumProgress: () => {},
 };
 
 export default Progress;
