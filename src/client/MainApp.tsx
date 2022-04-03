7fe6a937e12a0f3a68b8a713e03f0946d80b62e0
Minung Han <hmu332233@gmail.com>
2022-04-04 00:12:53 +0900

    feat: extension 설치 여부에 따라 mode가 결정되도록 변경

--- src/client/MainApp.tsx
+++ src/client/MainApp.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from './components/Button';
+
 
 import RecordPage from './pages/RecordPage';
 import Header from './components/Header';
 
-const extensionMode = process.env.EXTENSION_MODE === 'true';
+const extensionMode = !!chrome.runtime;
 function MainApp() {
   return (
     <>
