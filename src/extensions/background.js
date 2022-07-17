667c93cf0679161c2e0e7f29abc7b717ffb4ccc6
Minung Han <hmu332233@gmail.com>
2022-07-17 16:33:59 +0900

    feat: 익스텐션에서 사용 가능 여부 체크해주도록 추가

--- src/extensions/background.js
+++ src/extensions/background.js
@@ -1,7 +1,13 @@
 // sidebar -> background -> content-script 형식으로 데이터 내려주는 용도
-chrome.runtime.onMessageExternal.addListener(message => {
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    chrome.tabs.sendMessage(tabs[0].id, message);
-  });
+chrome.runtime.onMessageExternal.addListener((message, sender, sendResponse) => {
+  switch (message.action) {
+    case 'web-recorder-check':
+      sendResponse(true);
+      break;
+    default:
+      chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        chrome.tabs.sendMessage(tabs[0].id, message);
+      });
+  }
 });
 
