1235f29c8f11fd13ece0b5488556af081222fa6a
Minung Han <hmu332233@gmail.com>
2022-08-01 00:25:15 +0900

    fix: extensionId가 없을 경우 에러나던 버그 수정

--- src/client/utils/extension.ts
+++ src/client/utils/extension.ts
@@ -2,6 +2,10 @@ const extensionId: string = process.env.EXTENSION_ID || '';
 
 type sendMessageProps = { action: 'web-recorder-tools-active' | 'web-recorder-check', data?: object, callback?: ((response: any) => void) }
 const sendMessage = ({ action, data = {}, callback }: sendMessageProps) => { 
+  if (!extensionId) {
+    return;
+  }
+
   chrome?.runtime?.sendMessage?.(extensionId, { action, data }, callback);
 }
 
